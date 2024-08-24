55b6768e041daa361bf726b7a4846abd446e4e26
GooseDeBaas <129955449+GooseDeBaas@users.noreply.github.com>
2024-08-24 13:01:59 +0200

    Cleaning up code

--- test2.py
+++ test2.py
@@ -6,13 +6,6 @@ aantal_speeldagen = int((len(teams)-1)*2)
 aantal_teams = len(teams)
 matchdays = []
 
-# Creating starting set-up circle (Round Robin Tournament Algorithm)
-
-array1 = teams[:int(len(teams)/2)]
-array2 = teams[int((len(teams)/2)):int(len(teams))]
-reversed_array2 = array2[::-1]
-array2 = reversed_array2
-
 # Rotating function
 def rotate(array1, array2):
     team_to_first = array2.pop(0)
@@ -20,35 +13,35 @@ def rotate(array1, array2):
     team_to_last = array1.pop(len(array1)-1)
     array2.append(team_to_last)
 
-# Create first leg games and rotate
+# Shuffle teams
+random.shuffle(teams)
 
+# Creating starting set-up circle (Round Robin Tournament Algorithm)
+array1 = teams[:int(len(teams)/2)]
+array2 = teams[int((len(teams)/2)):int(len(teams))]
+reversed_array2 = array2[::-1]
+array2 = reversed_array2
+
+
+# Create first leg games
 for i in range(int((aantal_speeldagen)/2)):
     speeldag = []
     for j in range(int(aantal_teams/2)):
         heenronde = [array1[j], array2[j]]
-        terugronde = [array2[j], array1[j]]
         speeldag.append(heenronde)
     matchdays.append(speeldag)
-        
-
     rotate(array1, array2)
 
-# Create second leg games and rotate
-
+# Create second leg games
 for i in range(int((aantal_speeldagen)/2)):
     speeldag = []
     for j in range(int(aantal_teams/2)):
         terugronde = [array2[j], array1[j]]
         speeldag.append(terugronde)
     matchdays.append(speeldag)
-        
-
     rotate(array1, array2)
 
 
-# random.shuffle(matchdays)
-
-print(matchdays)
 
 # Print out random matchdays
 
